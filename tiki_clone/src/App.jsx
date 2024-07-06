ff48931efe3b652aba7070e901d4cf409df12158
Huy Lý Thành <lth121296@gmail.com>
2024-07-07 00:47:16 +0700

    first commit

---
+++ tiki_clone/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import BookPage from './pages/book';
+import ContactPage from './pages/contact';
+import { Outlet } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import LoginPage from './pages/login/LoginPage';
+import Register from './pages/register/Register';
+import "./styles/_reset.scss"
+
+const Layout = () => {
+  return (
+    <div className='layout-app'>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 not found hoi dan it</div>,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
---
+++ tiki_clone/src/pages/login/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./LoginPage.scss"
+
+const LoginPage = () => {
+    return (
+        <div>
+            login
+        </div>
+    );
+};
+
+export default LoginPage;
